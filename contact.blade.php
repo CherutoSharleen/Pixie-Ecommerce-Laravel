7af7ef58f56a8ae95c79cbb4e32f380333a31bb3
CherutoSharleen <92080989+CherutoSharleen@users.noreply.github.com>
2022-04-01 21:38:38 +0300

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,70 @@
+@extends('includes.layout')
+@section('content')
+    <!-- Page Content -->
+    <!-- About Page Starts Here -->
+    <div class="contact-page">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <div class="line-dec"></div>
+              <h1>Contact Us</h1>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div id="map">
+            		<!-- How to change your own map point
+                           1. Go to Google Maps
+                           2. Click on your location point
+                           3. Click "Share" and choose "Embed map" tab
+                           4. Copy only URL and paste it within the src="" field below
+                    -->
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8543489148915!2d36.834250350490294!3d-1.2595107990768275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f16e917335469%3A0xc202365459699789!2sMuthaiga%20Country%20Club!5e0!3m2!1sen!2ske!4v1648397244099!5m2!1sen!2ske" width="500" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-content">
+              <div class="container">
+                <form id="contact" action="" method="post">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control" id="email" placeholder="Your email..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">Send Message</button>
+                      </fieldset>
+                    </div>
+{{--                    <div class="col-md-12">--}}
+{{--                      <div class="share">--}}
+{{--                        <h6>You can also keep in touch on: <span><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-twitter"></i></a></span></h6>--}}
+{{--                      </div>--}}
+{{--                    </div>--}}
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    @stop
